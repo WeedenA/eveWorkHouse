29e1cdf9a16c9109b283a49662a1b60a19460efd
Alex Weeden <ajweeden2016@gmail.com>
2021-03-16 18:40:06 -0500

    clean up

--- miningBeltParse.py
+++ miningBeltParse.py
@@ -5,55 +5,33 @@ file = open('miningParse.txt')
 lines = file.readlines()
 file.close()
 # Assign Ore names
-# todo: parse column to add names as found (sort by variant percentage?)
+# todo: parse column to add names as found (sort by variant percentage?) dicts-Veldspar:0 Dense:1 etc
+# todo: multiple graphs to show densities etc side by side
 my_dict = {}
 # list of ores, total = index9
 oreList = ['Veldspar', 'Concentrated Veldspar', 'Dense Veldspar', 'Scordite',
            'Condensed Scordite', 'Massive Scordite', 'Pyroxeres', 'Solid Pyroxeres',
            'Viscous Pyroxeres', 'TotalDIV10']
-print(oreList)
-ores = [0,0,0,0,0,0,0,0,0,0]
+ores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+oreCount = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # splits lines, cleans extraneous text, pseudo-switch
 for line in lines:
     line = line.split('\t')
     line = [i.strip() for i in line]
-    #empt = np.append(empt, np.array([line]), axis=0)
     if (len(line) == 1):
         for x in range(2): line.append('0 m3')
     line[2] = line[2][:-3]
     line[2] = line[2].replace(',', '')
     intVol = int(line[2])
-    # kill me now, not after
-    if line[0] == oreList[0]:
-        ores[0] = ores[0] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[1]:
-        ores[1] = ores[1] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[2]:
-        ores[2] = ores[2] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[3]:
-        ores[3] = ores[3] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[4]:
-        ores[4] = ores[4] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[5]:
-        ores[5] = ores[5] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[6]:
-        ores[6] = ores[6] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[7]:
-        ores[7] = ores[7] + intVol
-        ores[9] = ores[9] + intVol
-    elif line[0] == oreList[8]:
-        ores[8] = ores[8] + intVol
-        ores[9] = ores[9] + intVol
+    for x in range(len(oreList)):
+        if line[0] == oreList[x]:
+            choice = x
+    ores[choice] = ores[choice] + intVol
+    oreCount[choice] += 1
+    ores[9] = ores[9] + intVol
 
-print(f"{ores}\n")  # final values before div
+
 maxOreVolume = (20000 - (max(ores[:9]) % 20000)) + max(ores[:9])  # rounded max ore to size graph
 total = ores[9]
 ores[9] = 0
@@ -81,14 +59,20 @@ plt.show()
 #
 # total v/s/p
 veld, scor, pyro = 0,0,0
+veldCount, scorCount, pyroCount = 0,0,0
 for x in range(3):
-    veld = ores[x] + veld
+    veld += ores[x]
+    veldCount += oreCount[x]
 for x in range(3,6):
-    scor = ores[x] + scor
+    scor += ores[x]
+    scorCount += oreCount[x]
 for x in range(6,9):
-    pyro = ores[x] + pyro
+    pyro += ores[x]
+    pyroCount += oreCount[x]
 comboOre = [veld, scor, pyro]
+comboCount = [veldCount, scorCount, pyroCount]
 comboOre = [int(x) for x in comboOre]
+oreDensity = np.divide(comboOre, comboCount)
 # begin terminal analysis
 cycleOre = np.divide(comboOre, 2714)
 cycleOre = cycleOre.astype(np.uint)
@@ -99,14 +83,18 @@ cycleOreMod = cycleOre.copy()
 cyclePC = sum(cycleOre) / 6
 for x in range(3):
     while (cycleOreMod[x] > cyclePC):
-        allocOre[x] = allocOre[x] + 1
+        allocOre[x] += 1
         cycleOreMod[x] = cycleOreMod[x] - cyclePC
+iOfMax = np.argmax(cycleOreMod)
+allocOre[iOfMax] += 1
 df = np.array([baseOreList, comboOre, cycleOre, timeOre, allocOre])
 for x in range(df.shape[1]):
     print(f"{df[0,x]}: {df[1,x]}m3, {df[2,x]} cycles, "
           f"{df[3,x]}min, allocate {df[4,x]}")
-freePilots = 6 - sum(allocOre)
-print(f"Free pilots: {freePilots}\t||\tCPC:{cyclePC}")
+
+print(oreCount)
+print(oreDensity)
+
 
 
 
