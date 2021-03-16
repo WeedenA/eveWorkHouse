04af3305ec6cb73fa2a410a7e0bb8141ba677f27
Alex Weeden <ajweeden2016@gmail.com>
2021-03-16 17:58:37 -0500

    qt push

--- sandbox.py
+++ sandbox.py
@@ -1,32 +1,13 @@
 import numpy as np
-oreList = ['Veldspar', 'Scordite', 'Pyro']
-ores = [100,200,300]
+from sympy import *
 
-test = np.array([oreList, ores])
-print(test)
-print(f"Veldspar: {test[:,0]}")
-
-
-comboOre = [veld, scor, pyro]
-comboOre = [int(x) for x in comboOre]
-# begin terminal analysis
-divOre = [i/1000 for i in comboOre]
-# print(f"Total Veldspar: {int(divOre[0])}km3")
-# print(f"Total Scordite: {int(divOre[1])}km3")
-# print(f"Total Pyroxeres: {int(divOre[2])}km3")
-cycleOre = np.divide(comboOre, 2714)
-cycleOre = cycleOre.astype(np.uint)
-timeOre = np.divide(comboOre, 2100).astype(np.uint)
-alloc = np.array([['Veldspar', 'Scordite', 'Pyroxeres'], cycleOre])
-for x in alloc: print(x)
-cyclePC = sum(cycleOre) / 6
+def eigen(X):
+    matrixMean = np.mean(X, axis=0)
+    cov = np.cov(X.T)
+    eigVal, eigVec = np.linalg.eig(cov)
+    print(f"EigenValue: {eigVal}\nEigenVector: {eigVec}")
 
 
-print("\nOre: Character Cycles / Character Time / Assigned Chars")
-print(f"Veldspar: {cycleOre[0]} cycles / {timeOre[0]} minutes / ")
-print(f"Scordite: {cycleOre[1]} cycles / {timeOre[1]} minutes / ")
-print(f"Pyroxeres: {cycleOre[2]} cycles / {timeOre[2]} minutes / ")
-# test, works :)
-# for x in range (len(ores)):
-#     print(f"{oreList[x]} : {ores[x]}")
-# What next?
+array1 = np.array([[4,1],[2,3],[5,4],[1,0]])
+
+eigen(array1)
